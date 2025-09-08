a9a1cec997bae9856f2aba51bd407d5b4529a9e6
TaeKyung Kwak <tae5431@gmail.com>
2025-09-08 14:20:10 +0900

    asdf

---
+++ col_0908_v2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<set>
+#include<cmath>
+#include <cstdlib>
+using namespace std;
+void cal(unsigned long long int n);
+unsigned int x=0;
+int main(){
+    unsigned long int x=0;
+    unsigned long long int n=19990012341299;
+    cal(n);
+    return 0;
+}
+unsigned long long int big_int=pow(2,60);
+void cal(unsigned long long int n){
+    x+=1;
+    if(n==1){
+        cout << "Be 1 in count : "<<x;
+        exit(0);
+    }
+    if(n%2==0){
+        cal(n>>1);
+    }
+    else{
+        if(n>=big_int){
+            cout << "count: "<<x<<" overnum is"<<n;
+            exit(0);
+        }
+        cal((n<<1)+n+1);
+    }
+}
--- free.cpp
+++ free.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
-    int i=9;
-    cout << (i<<1);
+    long long int i=pow(2,63);
+    int j=100;
+    cout << (j>>1);
 }
