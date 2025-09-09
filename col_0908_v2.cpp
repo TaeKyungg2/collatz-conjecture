6d849c4e67af9b2a1dcf83e47dff32e3ecf48b05
Taery <tae5431@gmail.com>
2025-09-09 17:16:29 +0900

    asdf

--- col_0908_v2.cpp
+++ col_0908_v2.cpp
@@ -2,22 +2,39 @@
 #include<set>
 #include<cmath>
 #include <cstdlib>
+#include <fstream>
+#include <cstring>
 using namespace std;
 void cal(unsigned long long int n);
 bool is_be_1[100000]={};
+unsigned long long int road[10000]={};
+ofstream outFile("col.txt");
 unsigned int x=0;
 int main(){
-    unsigned long int x=0;
-    unsigned long long int n=19990012341299;
-    cal(n);
+    //unsigned long long int n=2021232344;
+    for(unsigned long long int i=2;i<=10000;i++){
+        cal(i);
+        x=0;
+        memset(road, 0, sizeof(road));
+    }
+    outFile.close();
     return 0;
 }
-unsigned long long int big_int=100000;
+unsigned long long int big_int=100000000000000;
 void cal(unsigned long long int n){
     x+=1;
+    road[x]=n;
     if(n==1){
-        cout << "Be 1 in count : "<<x;
-        exit(0);
+        //outFile << "Be 1 in count : "<<x << endl;
+        for(int i =1;i<=10000;i++){
+            if(road[i]==0){
+            outFile<<endl;
+            return;
+            }
+            outFile << road[i] << ", ";
+        }
+        outFile << endl;
+        return;
     }
     if(n%2==0){
         cal(n>>1);
@@ -25,7 +42,7 @@ void cal(unsigned long long int n){
     else{
         if(n>=big_int){
             cout << "count: "<<x<<" overnum is"<<n;
-            exit(0);
+            return;
         }
         cal((n<<1)+n+1);
     }
