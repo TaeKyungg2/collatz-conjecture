172782234f926299e7af931afaac246065b6db5e
TaeKyung Kwak <tae5431@gmail.com>
2025-09-09 11:20:30 +0900

    adsf

--- colath.rs
+++ colath.rs
@@ -1,14 +1,16 @@
-static mut gat:[u64;1001]=[0;1001];
-static mut x:usize=0;
+
 fn main(){
-    unsafe{calcul(2);
+    mut gat:[u64;1001]=[0;1001];
+    mut x:usize=0;
+    calcul(2);
     gat.sort();
-    for i in gat{print!("{i}, ")};}
-}
-fn calcul(num:u64){
-    unsafe{if gat.contains(&num) || x>=1000 {return;}
-    x=x+1;
-    gat[x]=num;
-    if (num-1)%3==0{ calcul((num-1)/3);}
-    if num%2==0 {calcul(num<<1);}}
+    for i in gat{print!("{i}, ")};
+
+    fn calcul(num:u64){
+        if gat.contains(&num) || x>=1000 {return;}
+        x=x+1;
+        gat[x]=num;
+        if (num-1)%3==0{ calcul((num-1)/3);}
+        if num%2==0 {calcul(num<<1);}
+    }
 }
