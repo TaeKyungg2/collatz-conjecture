7194d20a604113712a99ef99c2dddccbc211dff6
Taery <tae5431@gmail.com>
2025-10-27 19:06:58 +0900

    asdf

--- parallel/src/main.rs
+++ parallel/src/main.rs
@@ -3,8 +3,10 @@ use std::io::{self, Write};
 use std::time::Instant;
 fn main(){
     let start = Instant::now();
-    let goal_num=1 << 5;
-    (3..goal_num).into_par_iter().filter(|x| x % 2 == 1).for_each(|i| {
+    let goal_num=1 << 28;
+    (3..goal_num).into_par_iter()
+    .filter(|x| x % 2 == 1)
+        .for_each(|i| {
         calculation(i);
         if i%(goal_num/16-1)==0{print!("{i} ");}
         io::stdout().flush().unwrap();
@@ -16,11 +18,7 @@ fn main(){
 fn calculation(i:u128){
     let mut n=i;
     while n>=i{
-        if n%2==0{
-            n=n>>1;
-        }
-        else{
-            n=n<<1+n+1;
-        }
+        if n%2==0{n=n>>1;}
+        else{n=(n<<1)+n+1;}
     }
 }
